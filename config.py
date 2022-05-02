0b7d337e66626c15d0351d2d530a02f17f7ddd72
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-02 23:58:12 +0300

    create a ProdConfig subclass with configurations used in the production stages.

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
-class config:
+class Config:
   '''
   General configuration parent class
   '''
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+
+class ProdConfig(Config):
+  '''
+  Production configuration child class
+
+  Args:
+      Config: The parent configuration class with general configuration settings
+  '''
+  pass
