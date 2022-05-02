e3fc7354ff682606cdcb366523285cfe3f08e5c1
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-02 18:42:07 +0300

    create a route decorator and define a view function.

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,9 @@
 from flask import render_template
 from . import main
+
+@main.route('/')
+def index():
+  '''
+  View root page function that returns the index page and its data
+  '''
+  return render_template('index.html')
