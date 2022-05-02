d43e6d9f51ea2e819ce70a0c112f1a1f9385d432
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-03 00:01:01 +0300

    create a subclass with configurations used in development stages of the app.

--- config.py
+++ config.py
@@ -12,3 +12,12 @@ class ProdConfig(Config):
       Config: The parent configuration class with general configuration settings
   '''
   pass
+
+class DevConfig(Config):
+  '''
+  Development configuration child class
+
+  Args:
+      Config: The parent configuration class with general configuration settings
+  '''
+  DEBUG = True
