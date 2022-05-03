5b50590d0660d63314d7b055270f27f78ea32156
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-04 01:23:23 +0300

    create a test class for the articles model class.

---
+++ tests/articles_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from app.models import Article
+
+class ArticlesTest(unittest.TestCase):
+  '''
+  Test Class to test the behaviour of the Articles class
+  '''
+  def setUp(self):
+    '''
+    Set up method that will run before every test
+    '''
+    self.new_article = Article('Severus Snape','Harry Potter defeats He who must not be named', 'Lorem ipsum dolor sit adipiscing elit.','https://www.wizardingworld.com/', 'https://www.wizardingworld.com/', '2022-05-02T08:45:18Z', 'Lorem ipsum dolor sit ')
+
+    def test_instance(self):
+      
+      self.assertTrue(isinstance(self.new_article,Article))
+
+    
