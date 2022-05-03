851f5454605c20deda50a3448db820af64a0e7df
lydiah1wachira <lydiahcedric@gmail.com>
2022-05-04 01:32:10 +0300

    create a model class for the articles.

--- tests/articles_test.py
+++ tests/articles_test.py
@@ -10,10 +10,10 @@ class ArticlesTest(unittest.TestCase):
     '''
     Set up method that will run before every test
     '''
-    self.new_article = Article('Severus Snape','Harry Potter defeats He who must not be named', 'Lorem ipsum dolor sit adipiscing elit.','https://www.wizardingworld.com/', 'https://www.wizardingworld.com/', '2022-05-02T08:45:18Z', 'Lorem ipsum dolor sit ')
+    self.new_article = Article('Severus Snape','Harry Potter defeats He who must not be named', 'Lorem ipsum dolor sit adipiscing elit.','https://www.wizardingworld.com/', 'https://www.wizardingworld.com/', '2022-05-02T08:45:18Z','gfygft7fqd wf gdcod c')
 
-    def test_instance(self):
-      
-      self.assertTrue(isinstance(self.new_article,Article))
+  def test_instance(self):
+    
+    self.assertTrue(isinstance(self.new_article,Article))
 
     
